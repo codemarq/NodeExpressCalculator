7df51b3878d5259e18c3ad8af7f98e15a46adc07
John-Mike Marquardt <jmmarquardt@gmail.com>
2016-10-18 19:31:39 -0400

    done

--- expressCalculator.js
+++ expressCalculator.js
@@ -6,9 +6,32 @@ var path = require('path');
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3010;
+var PORT = 3000;
 
+app.get('/:operator/:num1/:num2?', function (req, res) {
+	var operator = req.params.operator;
+	var num1 = parseFloat(req.params.num1);
+	var num2 = parseFloat(req.params.num2);
+	var result;
 
+	switch (operator) {
+		case "add":
+			result = num1 + num2;
+			break;
+		case "subtract":
+			result = num1 - num2;
+			break;
+		case "multiply":
+			result = num1 * num2;
+			break;
+		case "divide":
+			result = num1 / num2;
+			break;
+		default:
+			result = "you gotta learn math sucka! add, multiply, subtract, divide!!"
+	}
+	res.send(result.toString());
+});
 
 // Starts the server to begin listening
 // =============================================================
